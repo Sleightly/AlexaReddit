f3333a5f537f2d3dc73a673172db622bc3966497
Sleightly <ejc042@ucsd.edu>
2019-05-08 23:43:34 -0700

    adding body of reddit post

--- app.py
+++ app.py
@@ -16,34 +16,34 @@ def get_subreddits(text):
 	sess.post('https://www.reddit.com/api/login', data=user_pass)
 	time.sleep(1)
 
-	sub = 'leagueoflegends'
+	sub = 'jokes'
 	if text == '':
 		print('failed')
 		text = sub
 	
 	url = 'https://reddit.com/r/'+text+'/.json?limit=3'
-	print(url)
 	html = sess.get(url)
 	data = json.loads(html.content.decode('utf-8'))
-	titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
-
-	titles = '... next ... ...'.join([i for i in titles])
-	return 'reading subreddit '+ text+ "... ... ... " + titles
+	titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']][1:]
+	bodies = [unidecode.unidecode(listing['data']['selftext']) for listing in data['data']['children']][1:]
+	response = ''
+	for i, j in zip(titles, bodies):
+		response += i + '... ...' + j + '... ... ... ... next ... ...'
+	response = response[:-16]
+	return 'reading subreddit '+ text+ "... ... ... " + response
 
 titles = get_subreddits('')
-print(titles)
-
 
 @app.route('/')
 def home():
 	return 'Substitute homepage'
 
-'''@ask.launch
+@ask.launch
 def init_skill():
 	hello_msg = 'Opening Reddit, which sub reddit would you like to read?'
 	return question(hello_msg)
-'''
-@ask.launch
+
+#@ask.launch
 @ask.intent('CommandIntent')
 def read_any_subreddit(command):
 	command = command.strip()
@@ -52,7 +52,7 @@ def read_any_subreddit(command):
 	sub_reddit_msg = 'The found subreddits are {}'.format(subreddits)
 	return statement(sub_reddit_msg)
 
-'''
+
 @ask.intent("YesIntent")
 def read_subreddits():
 	print('here')
@@ -64,7 +64,7 @@ def read_subreddits():
 def stop_action():
 	bye = "Okay... bye"
 	return statement(bye)
-'''
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
